44be7e47df9bf4de08735491ed4c646cff053ee8
Ihor <igorechekqwerty@gmail.com>
2023-05-20 11:32:18 +0300

    Added project to repository

---
+++ configuration.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Testing the functionality to determine if the test build is working:', () => {
+  it('Should compare booleans', () => {
+    expect(true).toBe(true)
+  })
+})
